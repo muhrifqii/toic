3a4c4698564fd61e7d37a18c41432f690aced9ca
Muhammad Rifqi Fatchurrahman <muh_rif@live.com>
2025-05-02 16:12:18 +0700

    downgrade react to 18, uncompatible radixui/shadcnui with react 19

--- src/toic_frontend/src/App.tsx
+++ src/toic_frontend/src/App.tsx
@@ -1,8 +1,13 @@
 import { useState } from 'react'
 import { toic_backend } from '@declarations/toic_backend'
+import LandingPage from './pages/Landing'
 
 function App() {
-  return <></>
+  return (
+    <>
+      <LandingPage />
+    </>
+  )
 }
 
 export default App
